ea6e0fc296cd297975c0ce8132aad82bf437a346
tobryan1 <toddobryan@gmail.com>
2012-01-30 13:14:48 -0500

    login stuff

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,15 +1,16 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
-import play.api.data.{Form, of, text}
+import play.api.mvc.{Action, Controller, Session}
+import play.api.templates.Html
+import play.api.data.Form
+import play.api.data.Forms.{text, tuple}
 import views.html
-
 import models.users.User
 
 
 object Users extends Controller {
   val loginForm = Form(
-    of("username" -> text,
+    tuple("username" -> text,
        "password" -> text
     ) verifying ("Incorrect username or password.", result => result match {
       case (username, password) => User.authenticate(username, password).isDefined
@@ -40,5 +41,13 @@ object Users extends Controller {
     Redirect(routes.Users.login).withNewSession.flashing(
       "success" -> "You've been logged out"
     )
+  }
+  
+  def userInfo(implicit session: Session): Html = {
+    val maybeUser: Option[User] = session.get("username") match {
+      case Some(username) => User.getByUsername(username)
+      case None => None
+    }
+    html.userInfo(maybeUser)
   }
 }
