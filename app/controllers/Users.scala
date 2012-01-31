4eab7bf7c63bba61058e9442392fc13f6a5638d5
Todd O'Bryan <toddobryan@gmail.com>
2012-01-30 21:51:43 -0500

    Added a stylesheet, a list of all users, and made the login form look a lot nicer.

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -8,6 +8,7 @@ import views.html
 import models.users.User
 import util.DataStore
 import util.ScalaPersistenceManager
+import models.users.QUser
 
 object Users extends Controller {
   def loginForm(implicit pm: ScalaPersistenceManager) = Form(
@@ -46,5 +47,12 @@ object Users extends Controller {
     Redirect(routes.Users.login).withNewSession.flashing(
       "success" -> "You've been logged out"
     )
+  }
+  
+  def list = Action { implicit request =>
+    DataStore.withTransaction { implicit pm =>
+      val cand = QUser.candidate
+      Ok(html.users.list(pm.query[User].orderBy(cand.last.asc, cand.first.asc).executeList()))
+    }
   }
 }
