a4cfbaf15a4e01226e140afd6d60fc6b436ebc32
Todd O'Bryan <toddobryan@gmail.com>
2012-02-09 19:13:07 -0500

    added a "nucleus:enhance" command inside the "play" prompt run this after you run the "compile" command. I've tried to make it depend on the "compile" command so it will run it automatically, but no luck yet. On the other hand, at least you can get the code compiled and running without using Eclipse.

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,8 @@ import sbt._
 import Keys._
 import PlayProject._
 
+import java.io.File
+
 object ApplicationBuild extends Build {
 
     val appName         = "play-eschool"
@@ -23,7 +25,60 @@ object ApplicationBuild extends Build {
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+        Nucleus.settings: _*
     )
 
+}
+
+object Nucleus {
+  
+  // defines our own ivy config that wont get packaged as part of your app
+  // notice that it extends the Compile scope, so we inherit that classpath
+  val Config = config("nucleus") extend Compile
+
+  // our task
+  val enhance = TaskKey[Unit]("enhance")
+  
+  // implementation
+  val settings:Seq[Project.Setting[_]] = Seq(
+    // let ivy know about our "nucleus" config
+    ivyConfigurations += Config,
+    // add the enhancer dependency to our nucleus ivy config
+    libraryDependencies += "org.datanucleus" % "datanucleus-enhancer" % "3.0.1" % Config.name,
+    // fetch the classpath for our nucleus config
+    // as we inherit Compile this will be the fullClasspath for Compile + "datanucleus-enhancer" jar 
+    //fullClasspath in Config <<= (classpathTypes in enhance, update).map{(ct, report) =>
+    //  Classpaths.managedJars(Config, ct, report)
+    //},
+    // add more parameters as your see fit
+    //enhance in Config <<= (fullClasspath in Config, runner, streams).map{(cp, run, s) =>
+    enhance <<= Seq(compile in Compile).dependOn,
+    enhance in Config <<= (dependencyClasspath in Compile, classDirectory in Compile, runner, streams)
+        map { (deps, classes, run, s) => 
+
+      // Properties
+      val classpath = (deps.files :+ classes)
+      
+      
+      // the classpath is attributed, we only want the files
+      //val classpath = cp.files
+      // the options passed to the Enhancer... 
+      val options = Seq("-v") ++ findAllClassesRecursively(classes).map(_.getAbsolutePath)
+      
+      // run returns an option of errormessage
+      val result = run.run("org.datanucleus.enhancer.DataNucleusEnhancer", classpath, options, s.log)
+      // if there is an errormessage, throw an exception
+      result.foreach(sys.error)
+    }
+  )
+  
+  def findAllClassesRecursively(dir: File): Seq[File] = {
+    if (dir.isDirectory) {
+      dir.listFiles.flatMap(findAllClassesRecursively(_)) 
+    } else if (dir.getName.endsWith(".class")) {
+      Seq(dir)
+    } else {
+      Seq.empty
+    }
+  }
 }
