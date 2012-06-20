9a0012916055aca9df28d5c98a4bc87d4bc5fd70
Tyler Darnell <lokzor@gmail.com>
2012-06-20 10:20:39 -0400

    Created stubs for various blog functions. Awaiting model...

--- app/controllers/Blogs.scala
+++ app/controllers/Blogs.scala
@@ -7,7 +7,7 @@ import util.ScalaPersistenceManager
 import util.DbRequest
 import play.api.data._
 import play.api.data.Forms._
-
+import models.Users._
 
 object Blogs extends Controller {
   val newPost = Form {
@@ -24,6 +24,39 @@ object Blogs extends Controller {
 
   def editor() = DbAction { implicit req =>
     Ok(views.html.blogs.editor(testEdit))
+  }
+
+
+  /** List the blogs for a given user. If the user passed is the current user, the user can manage their blog.
+  *
+  *   @param user the user whose blogs to display
+  */
+  def listUserBlogs(user: User) = DbAction { implicit req =>
+    Ok(views.html.stub())
+  }
+
+  /** Show the control panel for a given blog. Checks to see if the correct user is stored in the session var first.
+  *
+  *   @param blog the blog to show the control panel for
+  */
+  def showControlPanel(blog: Blog) = DbAction { implicit req =>
+    Ok(views.html.stub())
+  }
+
+  /** Show a post. Check to see if the currently logged-in user is allowed to see the post
+  *
+  *   @param post the post to be shown
+  */
+  def showPost(post: Post) = DbAction { implicit req =>
+    Ok(views.html.stub())
+  }
+
+  /** Show a blog. Check to see if the currently logged-in user is allowed to view the blog
+  *
+  *   @param blog the blog to be shown
+  */
+  def showBlog(blog: Blog) = DbAction { implicit req =>
+    Ok(views.html.stub())
   }
 
   def testSubmit() = DbAction { implicit req =>
