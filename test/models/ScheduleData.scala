a740e85813501b105c5b38c8de3323ce4a3a4b3c
John King <johnfrancisking@gmail.com>
2012-07-27 21:35:25 -0400

    Finished all test data except for books stuff that isn't titles. OB will do the rest

--- test/models/ScheduleData.scala
+++ test/models/ScheduleData.scala
@@ -1,6 +1,6 @@
 package models.users
 import util.ScalaPersistenceManager
-import java.util.Date
+import java.sql.Date
 import org.joda.time.LocalDate
 import models.courses._
 import models.books._
@@ -13,6 +13,7 @@ object ScheduleData {
     //makeCourses(debug)
     //makeSections(debug)
     //makeEnrollments(debug)
+    //makeTeacherAssignments(debug)
     //makeBookData(debug)
     
     //create User Data
@@ -185,17 +186,46 @@ object ScheduleData {
       pm.makePersistentAll(List(enr, enr2))
     }
     
+    //makeTeacherAssignments
+    val maryAssignment = List(r1planning, r2usHistory, w1worldHistory, w2studySkill)
+    for(cls <- maryAssignment){
+      val enr = new TeacherAssignment(maryTeacher, cls, fall2012, null, null)
+      val enr2 = new TeacherAssignment(maryTeacher, cls, spring2013, null, null)
+      pm.makePersistentAll(List(enr, enr2))
+    }
+    
+    val tinaAssignment = List(r1english1, r2planning, w1english2, w2english3)
+    for(cls <- tinaAssignment){
+      val enr = new TeacherAssignment(christinaTeacher, cls, fall2012, null, null)
+      val enr2 = new TeacherAssignment(christinaTeacher, cls, spring2013, null, null)
+      pm.makePersistentAll(List(enr, enr2))
+    }
+    
+    val toddAssignment = List(r1algebra1, r2algebra2, w1planning, w2geometry)
+    for(cls <- toddAssignment){
+      val enr = new TeacherAssignment(toddTeacher, cls, fall2012, null, null)
+      val enr2 = new TeacherAssignment(toddTeacher, cls, spring2013, null, null)
+      pm.makePersistentAll(List(enr, enr2))
+    }
+    
+    val richAssignment = List(r1planning, r2usHistory, w1worldHistory, w2studySkill)
+    for(cls <- richAssignment){
+      val enr = new TeacherAssignment(richardTeacher, cls, fall2012, null, null)
+      val enr2 = new TeacherAssignment(richardTeacher, cls, spring2013, null, null)
+      pm.makePersistentAll(List(enr, enr2))
+    }
+    
     
     //makeBookData(debug)
-    val algebra1Book = new Title("Algebra 1 (Prentice Hall Mathematics)", "Pearson Prentice Hall", "978-0130523167", 842, "10.9 x 8.8 x 1.6 inches", 4.5, true, new Date())
-    val algebra2Book = new Title("Prentice Hall Mathematics: Algebra 2", "Pearson Prentice Hall", "978-0131339989", 842, "10.9 x 8.8 x 2 inches", 5.3, true, new Date())
-    val geometryBook = new Title("Geometry", "W.H. Freeman & Company", "978-0716717454", 668, "10.1 x 7.7 x 1.4 inches", 3.3, true, new Date())
-    val chemistryBook = new Title("Chemistry", "Houghton Mifflin", "978-0618528448", "1056", "10.6 x 8.5 x 1.6 inches", 5.5, true, new Date())
-    val biologyBook = new Title("Biology", " Pearson Benjamin Cummings", "978-0805368444", "1393", "8.5 x 2.1 x 11 inches", 7.6, true, new Date())
-    val english1Book = new Title("Glencoe Language Arts Grammar And Language Workbook Grade 9", " Glencoe/McGraw-Hill", "978-0028182940", "348", "10.9 x 8.5 x 0.6 inches", 9.6, true, new Date())
-    val english2Book = new Title("Prentice Hall Literature Penguin: Grade 10: Student Edition (NATL)", "PRENTICE HALL", "978-0131317185", "1163", "10 x 7.9 x 1.8 inches", 5.2, true, new Date())
-    val english3Book = new Title("Language of Literature, Grade 11 ", "McDougal Littel", "978-0395931813", "1408", "1 x 0.8 x 0.2 inches", 6.0, true, new Date())
-    val worldHistoryBook = new Title("World History: Patterns of Interaction: Atlas by Rand McNally", "Mcdougal Littell/Houghton Mifflin", "978-0618690084", "1376", "11.2 x 8.7 x 1.8 inches", 6.8, true, new Date())
-    val usHistoryBook = new Title("The American Pageant", "Wadsworth Publishing", "978-1111349530", "1152", "11 x 8.8 x 1.6 inches", 5.2, true, new Date())
+    val algebra1Book = new Title("Algebra 1 (Prentice Hall Mathematics)", "Bellman, Bragg and Charles", "Pearson Prentice Hall", "978-0130523167", 842, "10.9 x 8.8 x 1.6 inches", 4.5, true, new Date(System.currentTimeMillis()))
+    val algebra2Book = new Title("Prentice Hall Mathematics: Algebra 2", "Dan Kennedy, Randall I. Charles and Sadie Chavis Bragg", "Pearson Prentice Hall", "978-0131339989",900, "10.9 x 8.8 x 2 inches", 5.3, true, new Date(System.currentTimeMillis()))
+    val geometryBook = new Title("Geometry", "Harold R. Jacobs", "W.H. Freeman & Company", "978-0716717454", 668, "10.1 x 7.7 x 1.4 inches", 3.3, true, new Date(System.currentTimeMillis()))
+    val chemistryBook = new Title("Chemistry", "Steven S. Zumdahl and Susan A. Zumdahl", "Houghton Mifflin", "978-0618528448", 1056, "10.6 x 8.5 x 1.6 inches", 5.5, true, new Date(System.currentTimeMillis()))
+    val biologyBook = new Title("Biology", "Neil A. Campbell, Jane B. Reece, Lisa A. Urry and Michael L. Cain", " Pearson Benjamin Cummings", "978-0805368444", 1393, "8.5 x 2.1 x 11 inches", 7.6, true, new Date(System.currentTimeMillis()))
+    val english1Book = new Title("Glencoe Language Arts Grammar And Language Workbook Grade 9", "John King", " Glencoe/McGraw-Hill", "978-0028182940", 348, "10.9 x 8.5 x 0.6 inches", 9.6, true, new Date(System.currentTimeMillis()))
+    val english2Book = new Title("Prentice Hall Literature Penguin: Grade 10: Student Edition (NATL)", "Todd O'Bryan", "PRENTICE HALL", "978-0131317185", 1163, "10 x 7.9 x 1.8 inches", 5.2, true, new Date(System.currentTimeMillis()))
+    val english3Book = new Title("Language of Literature, Grade 11 ", "Pat Day", "McDougal Littel", "978-0395931813", 1408, "1 x 0.8 x 0.2 inches", 6.0, true, new Date(System.currentTimeMillis()))
+    val worldHistoryBook = new Title("World History: Patterns of Interaction: Atlas by Rand McNally", "Roger B. Beck, Linda Black and Larry S. Krieger", "Mcdougal Littell/Houghton Mifflin", "978-0618690084", 1376, "11.2 x 8.7 x 1.8 inches", 6.8, true, new Date(System.currentTimeMillis()))
+    val usHistoryBook = new Title("The American Pageant", "David M. Kennedy and Lizabeth Cohen", "Wadsworth Publishing", "978-1111349530", 1152, "11 x 8.8 x 1.6 inches", 5.2, true, new Date(System.currentTimeMillis()))
   }
 }
