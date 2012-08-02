85e897698a779b5a1c816e55fc5324b6fbe99f42
Todd O'Bryan <toddobryan@gmail.com>
2012-08-02 08:39:06 -0400

    Trying to get Selenium tests working

--- test/app/TestLogins.scala
+++ test/app/TestLogins.scala
@@ -1,15 +1,17 @@
 package app
 
 import org.specs2.mutable._
+import org.openqa.selenium.chrome._
 
 import play.api.test._
 import play.api.test.Helpers._
 
 class TestLogins extends Specification {
   "run in a browser" in {
-    running(TestServer(3333), HTMLUNIT) { browser =>
+    System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, "/opt/chromedriver")
+    running(TestServer(3333), classOf[ChromeDriver]) { browser =>
       browser.goTo("http://localhost:3333/")
-      browser.$("#title").getTexts().get(0) must equalTo("Hello Guest")
+      browser.$("title").first.getText must equalTo("JCPS eSchool")
     }
   }
 }
