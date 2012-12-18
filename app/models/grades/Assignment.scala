65f9f4b175d2bcaaa88c250c0267b12cc40c2942
John King <johnfrancisking@gmail.com>
2012-12-18 13:44:33 -0500

    Saved the models because I forgot to previously

--- app/models/grades/Assignment.scala
+++ app/models/grades/Assignment.scala
@@ -1,5 +1,20 @@
 package models.grades
 
-class Assignment {
+import java.sql.Date
+import javax.jdo.annotations._
+import org.datanucleus.api.jdo.query._
+import org.datanucleus.query.typesafe._
+import util.ScalaPersistenceManager
+import util.PersistableFile
+import util.DataStore
 
+@PersistenceCapable(detachable="true")
+class Assignment {
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.INCREMENT)
+	private[this] var _id: Long = _
+	private[this] var _dateDue: java.sql.Date = _
+	private[this] var _datePosted: java.sql.Date = _
+	private[this] var _name: String = _
+	private[this] var _points: Int = _
 }
--- app/models/grades/Grade.scala
+++ app/models/grades/Grade.scala
@@ -1,5 +1,20 @@
 package models.grades
 
-class Grade {
+import javax.jdo.annotations._
+import org.datanucleus.api.jdo.query._
+import org.datanucleus.query.typesafe._
+import util.ScalaPersistenceManager
+import util.PersistableFile
+import util.DataStore
+import models.users.Student //or just models.users?
 
+@PersistenceCapable(detachable="true")
+class Grade {
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.INCREMENT)
+	private[this] var _id: Long = _
+	private[this] var _student: Student = _
+	private[this] var _assignment: Assignment = _
+	private[this] var _points: Double = _
+  
 }
