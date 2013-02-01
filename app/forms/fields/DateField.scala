5ed14364cc618304005850089361588306e12647
Allen Boss <allenboss95@gmail.com>
2013-01-31 20:10:34 -0500

    worked on getting datefields to work

---
+++ app/forms/fields/DateField.scala
@@ -0,0 +1,20 @@
+package forms.fields
+import forms.validators.ValidationError
+import java.sql.Date
+
+class DateField(name: String) extends Field[Date](name) {
+  override def required = true
+
+  def asValue(s: Seq[String]): Either[ValidationError, Date] =
+    try {
+      val splitdate = s(0).split("/")
+
+      if (splitdate(0).length == 1) splitdate(0) = "0" + splitdate(0)
+      if (splitdate(1).length == 1) splitdate(1) = "0" + splitdate(1)
+      if (splitdate(2).length == 2) Left(ValidationError("make sure you input a 4 digit year"))
+
+      Right(Date.valueOf(splitdate(2) + "-" + splitdate(0) + "-" + splitdate(1)))
+    } catch {
+      case _ => Left(ValidationError("Please make sure input is in mm/dd/yyyy format."))
+    }
+}
