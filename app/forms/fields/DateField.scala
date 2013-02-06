1180cf24e9e6e00f4c2b8d3f2bb45ad148e366e5
Ken Hayes <hayes.ken.m@gmail.com>
2013-02-06 13:38:32 -0500

    Created DateFieldOptional and tested it

--- app/forms/fields/DateField.scala
+++ app/forms/fields/DateField.scala
@@ -3,11 +3,15 @@ import forms.validators.ValidationError
 import java.sql.Date
 import forms.widgets.DateInput
 
-class DateField(name: String) extends Field[Date](name) {
+
+abstract class BaseDateField[T](name: String)(implicit man: Manifest[T]) extends Field[T](name) {
   
   val uuid=java.util.UUID.randomUUID()
   
   override def widget = new DateInput(required, uuid=uuid)
+}
+
+class DateField(name: String) extends BaseDateField[Date](name) {
   
   def asValue(s: Seq[String]): Either[ValidationError, Date] =
     try {
@@ -21,4 +25,24 @@ class DateField(name: String) extends Field[Date](name) {
     } catch {
       case _ => Left(ValidationError("Please make sure input is in mm/dd/yyyy format."))
     }
+}
+
+class DateFieldOptional(name: String) extends BaseDateField[Option[Date]](name) {
+	override def required = false
+	
+	def asValue(s: Seq[String]): Either[ValidationError, Option[Date]] =
+	  s match {
+	  case Seq() => Right(None)
+	  case Seq(str) => try {
+      val splitdate = s(0).split("/")
+
+      if (splitdate(0).length == 1) splitdate(0) = "0" + splitdate(0)
+      if (splitdate(1).length == 1) splitdate(1) = "0" + splitdate(1)
+      if (splitdate(2).length == 2) Left(ValidationError("make sure you input a 4 digit year"))
+
+      Right(Option[Date](Date.valueOf(splitdate(2) + "-" + splitdate(0) + "-" + splitdate(1))))
+    } catch {
+      case _ => Left(ValidationError("Please make sure input is in mm/dd/yyyy format."))
+    }
+	}
 }
