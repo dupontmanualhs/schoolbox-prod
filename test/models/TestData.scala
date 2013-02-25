30ad812c6cfbc9e51cc5c9b4fc1bb5fb2a91c455
Sam Fu <sam@s-SatL-x.(none)>
2013-02-24 20:41:42 -0500

    Turns out TestData compile error is in master. I'll change that next.

--- test/models/TestData.scala
+++ test/models/TestData.scala
@@ -267,7 +267,7 @@ object TestData {
     if(debug) println("Creating Conferences...")
     val springConf = new Event("Spring Conferences", true)
     val springSession = new Session(springConf, new Date(2013, 4, 1), new Timestamp(2013, 4, 21, 23, 59, 59, 99), 
-        new Timestamp(2013, 4, 1, 23, 59, 59, 99), new Time(0, 0, 0), new Time(23, 59, 59), 10)
+        Some(new Timestamp(2013, 4, 1, 23, 59, 59, 99)), new Time(0, 0, 0), new Time(23, 59, 59), 10)
     val firstSlot = new Slot(springSession, Teacher.getByUsername("736052").asInstanceOf[Teacher] /*ob*/, Student.getByUsername("RASHAH01").asInstanceOf[Student], 
         new Time(12, 00, 00), "Mark Shah", "fakeemail@n00b.com", "5025555555", null, null)
   }
