781405fb727e3aacbd61cf12ef802060e28de930
Todd O'Bryan <toddobryan@gmail.com>
2013-03-03 18:58:10 -0500

    deleted the .metadata and .worksheet folders, which somehow got added

--- .worksheet/src/models.users.test.scala
+++
@@ -1,6 +0,0 @@
-package models.users
-import models.users.Teacher._
-import models.users.Student._
-object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(130); val res$0 = 
-  Student.getByUsername("RASHAH01");System.out.println("""res0: Option[models.users.Student] = """ + $show(res$0))}
-}
