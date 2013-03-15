98e8125e8316b58d60558bf5843178edd1404072
John King <johnfrancisking@gmail.com>
2013-03-15 14:19:17 -0400

    Fixed results problem from splitting assignments and started on perspectives and privacy stuff

--- app/controllers/Grades.scala
+++ app/controllers/Grades.scala
@@ -15,6 +15,7 @@ import util.DbRequest
 import util.ScalaPersistenceManager
 import scala.xml.Text
 import util.Authenticated
+import play.api.mvc.Result
 
 object Grades extends Controller {
 
@@ -24,78 +25,82 @@ object Grades extends Controller {
     val numPoints = new forms.fields.NumericField[Int]("Points Possible")
     val dueDate = new DateField("DueDate")
     val locked = new DateField("Hidden Until")
-    
+
     override val cancelTo = "url"
-    
+
     val fields = List(category, title, numPoints, dueDate, locked)
   }
 
   def assignments(sectionId: Long) = Authenticated { implicit req =>
     val persp = req.visit.perspective.get
     persp match {
-      case _:Teacher => assignmentsForTeachers(sectionId)(req)
-     // case _:Student => assignmentsForStudents(sectionId)
+      case teacher: Teacher => assignmentsForTeachers(sectionId, teacher)(req).asInstanceOf[PlainResult] //TODO fix this cast
+      // case _:Student => assignmentsForStudents(sectionId)
     }
-    
+
   }
-  
-  def assignmentsForTeachers(sectionId: Long) = DbAction { implicit req =>
+
+  def assignmentsForTeachers(sectionId: Long, teacher: Teacher) = DbAction { implicit req =>
     implicit val pm: ScalaPersistenceManager = req.pm
     val cand = QSection.candidate
     pm.query[Section].filter(cand.id.eq(sectionId)).executeOption() match {
       case None => NotFound(views.html.notFound("No section with that id."))
       case Some(sect) => {
-        val cats = Category.forSection(sect)
-        val catsMap = Category.forSection(sect).map(c => (c.name, c))
-        val dropMenu = new DropMenu(catsMap)
-        if (req.method == "GET") {
-          Ok(html.grades.assignments(sect, cats, Binding(dropMenu), sectionId))
-        } else Binding(dropMenu, req) match {
-          case ib: InvalidBinding => Ok(views.html.grades.assignments(sect, cats, ib, sectionId))
-          case vb: ValidBinding => {
-            val TheCat: Category = vb.valueOf(dropMenu.category)
-            val TheTitle: String = vb.valueOf(dropMenu.title)
-            val ThePoints: Int = vb.valueOf(dropMenu.numPoints)
-            val TheDueDate: java.sql.Date = vb.valueOf(dropMenu.dueDate)
-            val TheLocked: java.sql.Date = vb.valueOf(dropMenu.locked)
-            val assignment = new Assignment(TheTitle, ThePoints, TheDueDate, TheLocked, TheCat)
-            pm.makePersistent(assignment)
-            Redirect(routes.Grades.assignments(sectionId))
+        if (!sect.teachers.contains(teacher)) {
+          NotFound(views.html.notFound("You do not have permisson to view this course."))
+        } else {
+          val cats = Category.forSection(sect)
+          val catsMap = Category.forSection(sect).map(c => (c.name, c))
+          val dropMenu = new DropMenu(catsMap)
+          if (req.method == "GET") {
+            Ok(html.grades.assignments(sect, cats, Binding(dropMenu), sectionId))
+          } else Binding(dropMenu, req) match {
+            case ib: InvalidBinding => Ok(views.html.grades.assignments(sect, cats, ib, sectionId))
+            case vb: ValidBinding => {
+              val TheCat: Category = vb.valueOf(dropMenu.category)
+              val TheTitle: String = vb.valueOf(dropMenu.title)
+              val ThePoints: Int = vb.valueOf(dropMenu.numPoints)
+              val TheDueDate: java.sql.Date = vb.valueOf(dropMenu.dueDate)
+              val TheLocked: java.sql.Date = vb.valueOf(dropMenu.locked)
+              val assignment = new Assignment(TheTitle, ThePoints, TheDueDate, TheLocked, TheCat)
+              pm.makePersistent(assignment)
+              Redirect(routes.Grades.assignments(sectionId))
+            }
           }
         }
       }
     }
   }
-  
+
   def deleteAssignment(sectionId: Long, assignmentId: Long) = DbAction { implicit req =>
     implicit val pm: ScalaPersistenceManager = req.pm
     pm.query[Assignment].filter(QAssignment.candidate.id.eq(assignmentId)).executeOption() match {
       case None => NotFound(views.html.notFound("No assignment with that id."))
       case Some(assign) => {
         pm.deletePersistent(assign)
-         Redirect(routes.Grades.assignments(sectionId))
+        Redirect(routes.Grades.assignments(sectionId))
       }
     }
   }
 
-  def home(sectionId: Long) = DbAction { implicit req => 
+  def home(sectionId: Long) = DbAction { implicit req =>
     implicit val pm: ScalaPersistenceManager = req.pm
     val cand = QSection.candidate
     pm.query[Section].filter(cand.id.eq(sectionId)).executeOption() match {
       case None => NotFound(views.html.notFound("No section with that id."))
       case Some(sect) => {
-    	 Ok(views.html.grades.home(sectionId, sect))
+        Ok(views.html.grades.home(sectionId, sect))
       }
     }
   }
 
-  def announcements(sectionId: Long) = DbAction { implicit req => 
+  def announcements(sectionId: Long) = DbAction { implicit req =>
     implicit val pm: ScalaPersistenceManager = req.pm
     val cand = QSection.candidate
     pm.query[Section].filter(cand.id.eq(sectionId)).executeOption() match {
       case None => NotFound(views.html.notFound("No section with that id."))
       case Some(sect) => {
-    	 Ok(views.html.grades.announcements(sectionId, sect))
+        Ok(views.html.grades.announcements(sectionId, sect))
       }
     }
   }
