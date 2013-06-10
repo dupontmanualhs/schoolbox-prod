e63b43064689f73d011038bedaf8c6dd2b665089
Todd O'Bryan <toddobryan@gmail.com>
2013-06-09 22:00:06 -0400

    modified <b class="caret"> </b> so it wouldn't get turned into empty tag

--- app/util/Menu.scala
+++ app/util/Menu.scala
@@ -1,28 +1,27 @@
 package util
 
-import scala.xml.{Elem, NodeSeq}
+import scala.xml.{ Elem, NodeSeq }
 import models.users.Perspective
 
 class MenuItem(val name: String, val id: String, val link: Option[String], val subItems: List[MenuItem]) {
   def asHtml: Elem = if (subItems.isEmpty) {
-    					<li><a href={ link.getOrElse("#") } id={ id }>{ name }</a>
-    					</li>
-    					}
-                         else{
-                         <li class="dropdown" >
-                    	   <a class="dropdown-toggle" data-toggle="dropdown" href="#" >
-  								{ name }
-  								<b class="caret"> </b>
-  							</a>
-  							<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-  								<li>
-  									{ subItems.flatMap(_.asHtml) }
-                       		    </li>
-                       	   </ul>
-                       	</li>
-                         }
+    <li>
+      <a href={ link.getOrElse("#") } id={ id }>{ name }</a>
+    </li>
+  } else {
+    <li class="dropdown">
+      <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+        { name }
+        <b class="caret"> </b>
+      </a>
+      <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+        <li>
+          { subItems.flatMap(_.asHtml) }
+        </li>
+      </ul>
+    </li>
+  }
 }
-
 
 class MenuBar(val menus: List[MenuItem]) {
   def asHtml: Elem = <ul class="nav">{ menus.flatMap(_.asHtml) }</ul>
@@ -36,11 +35,11 @@ object Menu {
   val findlocnum = new MenuItem("Find Locker by Number", "menu_lockerNum", Some(controllers.routes.Lockers.lockerByNumber().toString), Nil)
   val locsearch = new MenuItem("Find Locker", "menu_lockerSearch", Some(controllers.routes.Lockers.lockerSearch().toString), Nil)
   val locsched = new MenuItem("Find Locker by Class", "menu_lockerClass", Some(controllers.routes.Lockers.schedule().toString), Nil)
-  val addTitle = new MenuItem("Add a Title", "menu_addTitle" , Some(controllers.routes.Books.addTitle().toString), Nil)
+  val addTitle = new MenuItem("Add a Title", "menu_addTitle", Some(controllers.routes.Books.addTitle().toString), Nil)
   val chkHistory = new MenuItem("Checkout History", "menu_chkHistory", Some(controllers.routes.Books.findCheckoutHistory.toString), Nil)
   val copyHistory = new MenuItem("Copy History", "menu_copyHistory", Some(controllers.routes.Books.findCopyHistory.toString), Nil)
-  val currentBks = new MenuItem("Current Checkouts", "menu_currentBks", Some(controllers.routes.Books.findCurrentCheckouts.toString), Nil)     
-    
+  val currentBks = new MenuItem("Current Checkouts", "menu_currentBks", Some(controllers.routes.Books.findCurrentCheckouts.toString), Nil)
+
   def buildMenu(persp: Option[Perspective]): Elem = {
     val acctItems = if (persp.isDefined) List(logout, settings) else List(login)
     val locItems = List(currloc, locsearch, locsched, findlocnum)
