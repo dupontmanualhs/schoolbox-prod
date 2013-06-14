43692ff2af309820e15a478844034bc926efe20b
Allen Boss <azblitz@gmail.com>
2013-06-14 15:08:15 -0400

    pushing before I leave.

--- app/forms/fields/AutocompleteField.scala
+++ app/forms/fields/AutocompleteField.scala
@@ -10,21 +10,21 @@ abstract class BaseAutocompleteField[T](name: String, list: List[Any])(implicit 
   
   override def widget = new AutocompleteInput(required, toJsArray(list), uuid=uuid)
   
-  def toJsArray(s: List[Any]): String = {
-    val strings= "[" + s.map(x => "\""+x.toString+"\"").reduce(_+","+_) + "]"
-    System.out.println(strings)
-    strings
-  }
+  def toJsArray(s: List[Any]): String = "[" + s.map(x => "\""+x.toString+"\"").reduce(_+","+_) + "]"
 }
 
 class AutocompleteField(name: String, list: List[Any]) extends BaseAutocompleteField[String](name, list) {
   
-  def asValue(s: Seq[String]): Either[ValidationError, String] = Right("")
+  def asValue(s: Seq[String]): Either[ValidationError, String] = 
+    if(s.isEmpty) Left(ValidationError("This Field is Required"))
+    else Right(s(0))
 
 }
 
 class AutocompleteFieldOptional(name: String, list: List[Any]) extends BaseAutocompleteField[Option[String]](name, list) {
   override def required = false
   
-    def asValue(s: Seq[String]): Either[ValidationError, Option[String]] = Right(None)
+    def asValue(s: Seq[String]): Either[ValidationError, Option[String]] = 
+      if(s.isEmpty) Right(None)
+      else Right(Some(s(0)))
 }
