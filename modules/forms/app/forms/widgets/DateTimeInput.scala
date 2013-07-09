09d448f1e726e9ba4628b58fc4fb337f0b3e49b9
Allen Boss <azblitz@gmail.com>
2013-07-09 12:52:55 -0400

    I just keep changing things

--- modules/forms/app/forms/widgets/DateTimeInput.scala
+++ modules/forms/app/forms/widgets/DateTimeInput.scala
@@ -13,35 +13,8 @@ class DateTimeInput(
   val timeWidget = new TimeInput(required, attrs)
   
   def render(name: String, value: Seq[String], attrList: MetaData = Null) = {
-    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"/>
-    <link rel="stylesheet" type="text/css" media="screen" href="http://tarruda.github.com/bootstrap-datetimepicker/assets/css/bootstrap-datetimepicker.min.css"/>
-    <div id="datetimepicker" class="input-append date">
-      <input type="text"></input>
-      <span class="add-on">
-        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-      </span>
-    </div>
-    <script type="text/javascript"
-     src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js">
-    </script> 
-    <script type="text/javascript"
-     src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js">
-    </script>
-    <script type="text/javascript"
-     src="http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.min.js">
-    </script>
-    <script type="text/javascript"
-     src="http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.pt-BR.js">
-    </script>
-    <script type="text/javascript">
-      $('#datetimepicker').datetimepicker({{
-        format: 'MM/dd/yyyy hh:mm',
-        language: 'pt-BR',
-	  	pickSeconds: false,
-	  	pick12HourFormat: true
-      }});
-    </script>
+    dateWidget.render(name, value, attrList) ++ <br/> ++ timeWidget.render(name, value, attrList)
   }
   
-  //override def scripts: NodeSeq = dateWidget.scripts ++ timeWidget.scripts
+  override def scripts: NodeSeq = dateWidget.scripts ++ timeWidget.scripts
 }
