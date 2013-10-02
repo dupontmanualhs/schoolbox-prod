10a8a393e3c90c87b183b90c0cfa5b3dad675152
Todd O'Bryan <toddobryan@gmail.com>
2013-10-01 21:49:02 -0400

    added menus to send activation emails, etc.

--- app/controllers/Menu.scala
+++ app/controllers/Menu.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import scala.xml.{ Elem, NodeSeq }
-import models.users.Role
+import models.users.{ Role, User }
 import controllers.courses.{ Menu => CoursesMenu }
 import config.users.{ Menu => UserMenu }
 import config.{ BookMenu, ConferencesMenu }
@@ -12,8 +12,29 @@ object Menu {
     val books: Option[MenuItem] = BookMenu.forRole(maybeRole)
     val courses: Option[MenuItem] = CoursesMenu.forRole(maybeRole)
     val confs: Option[MenuItem] = ConferencesMenu.forRole(maybeRole)
-    val bar = new MenuBar(List(courses, books, confs).flatten)
+    val admin: Option[MenuItem] = AdminMenu.forRole(maybeRole)
+    val bar = new MenuBar(List(courses, books, confs, admin).flatten)
     
     bar.asHtml ++ <div class="pull-right">{ UserMenu.forRole(maybeRole).asHtml }</div>
+  }
+}
+
+object AdminMenu {
+  val updateUser = new MenuItem("Update User", "menu_updateUser", Some(controllers.users.routes.App.chooseUserToEdit().toString), Nil)  
+  val sendTeacherActivation = new MenuItem("Send Teacher Activation",
+      "menu_teacherActivation", Some(controllers.routes.MoveMe.sendTeacherActivation().toString), Nil)
+  val sendGuardianActivation = new MenuItem("Send Guardian Activation",
+      "menu_guardianActivation", Some(controllers.routes.MoveMe.sendGuardianActivation().toString), Nil)
+
+  def forRole(maybeRole: Option[Role]) = maybeRole match {
+    case Some(role) if (role.permissions().contains(User.Permissions.Manage)) => {
+      val items = List(
+        updateUser,
+        sendTeacherActivation,
+        sendGuardianActivation
+      )
+      Some(new MenuItem("Admin", "menu_admin", None, items))
+    }
+    case _ => None
   }
 }
